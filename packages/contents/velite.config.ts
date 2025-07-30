a5615fda7447a792af2bb5988a56dd40c0662e1e
Hieu-BuiMinh <minhhieu2122000@gmail.com>
2025-07-31 00:09:22 +0700

    [feat]-dev: add tailwind typhography, clone clerk code blocks

--- packages/contents/velite.config.ts
+++ packages/contents/velite.config.ts
@@ -1,6 +1,11 @@
 import rehypeShiki from '@shikijs/rehype'
+import {
+	transformerNotationDiff,
+	transformerNotationErrorLevel,
+	transformerNotationFocus,
+	transformerNotationHighlight,
+} from '@shikijs/transformers'
 import rehypeAutoLinkHeading from 'rehype-autolink-headings'
-import rehypeHighlight from 'rehype-highlight'
 import rehypeSlug from 'rehype-slug'
 import { defineConfig } from 'velite'
 
@@ -21,8 +26,28 @@ export default defineConfig({
 	mdx: {
 		rehypePlugins: [
 			rehypeSlug,
-			[rehypeShiki, { themes: { dark: 'night-owl', light: 'github-light', keepBackground: false } }],
-			[rehypeHighlight],
+			[
+				rehypeShiki,
+				{
+					themes: {
+						dark: 'night-owl',
+						light: 'github-light',
+						keepBackground: false,
+					},
+
+					transformers: [
+						// https://shiki.style/packages/transformers#matchalgorithm-v3
+						transformerNotationDiff({ matchAlgorithm: 'v3' }),
+						transformerNotationHighlight({
+							matchAlgorithm: 'v3',
+							classActiveLine: 'code-block__line--highline',
+							classActivePre: 'code-block__pre--highline',
+						}),
+						transformerNotationFocus({ matchAlgorithm: 'v3' }),
+						transformerNotationErrorLevel({ matchAlgorithm: 'v3' }),
+					],
+				},
+			],
 			[
 				rehypeAutoLinkHeading,
 				{
