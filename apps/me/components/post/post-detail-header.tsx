2c4954e13caa769d8f4376170b7cb5f3d71eab5a
Hieu-BuiMinh <minhhieu2122000@gmail.com>
2025-07-31 23:46:30 +0700

    fix(post-title): render post title visibility

--- apps/me/components/post/post-detail-header.tsx
+++ apps/me/components/post/post-detail-header.tsx
@@ -14,12 +14,13 @@ function DocDetailHeader({ post, className }: IDocDetailHeaderProps) {
 
 	return (
 		<div className={cn('relative flex flex-col gap-5 border-b border-dashed pb-5', className)}>
-			<div className="h-[230px] md:min-h-[630px]">
+			<div className="min-h-[230px] md:min-h-[630px]">
 				<ImageZoom
 					alt="cover"
 					src={cover || ''}
-					width={1200}
+					width={1300}
 					height={630}
+					unoptimized={false}
 					className="dot-cover relative h-[230px] md:h-[630px] w-full rounded-lg hidden md:block"
 				/>
 				<h1 className="z-10 bg-gradient-to-b from-black via-black/90 to-black/70 to-90% bg-clip-text py-2 text-center text-4xl font-bold text-transparent dark:from-white dark:via-white/90 dark:to-white/70 md:text-5xl md:leading-[64px]">
