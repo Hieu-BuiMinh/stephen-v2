9460e7fda7affd31275e9cc718813ca290f65c8d
Hieu-BuiMinh <minhhieu2122000@gmail.com>
2025-08-12 00:34:20 +0700

    feat(connections): add connection block

--- apps/me/view/connections/pages/connections.page.tsx
+++ apps/me/view/connections/pages/connections.page.tsx
@@ -1,25 +1,31 @@
 import { AnimatedBlock } from '@repo/stephen-v2-ui/motion'
+import { d } from '@repo/stephen-v2-utils'
 
 import ConnectionBlock from '@/view/connections/components/connection-block'
 import { MyConnections } from '@/view/connections/constants/connections'
 
 function ConnectionsPageView() {
-	const sortedConnections = [...MyConnections].sort((a, b) => {
-		if (a.isConnected === b.isConnected) {
-			return a.name.localeCompare(b.name)
+	const connections = [...MyConnections].sort((a, b) => {
+		// put items with a connectedDate first
+		if (a.connectedDate && !b.connectedDate) return -1
+		if (!a.connectedDate && b.connectedDate) return 1
+
+		// if both have a connectedDate -> sort by date (newest first)
+		if (a.connectedDate && b.connectedDate) {
+			return d(b.connectedDate).valueOf() - d(a.connectedDate).valueOf()
 		}
-		return a.isConnected ? -1 : 1
+
+		// if neither has a connectedDate -> keep original order
+		return 0
 	})
 
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 xl:grid-cols-9 max-[280px]:grid-cols-1 gap-3 pb-24">
-			{sortedConnections.map((item, i) => {
-				return (
-					<AnimatedBlock type="FADE_IN" delay={0.1 * i} className="col-span-1" key={i}>
-						<ConnectionBlock data={item} />
-					</AnimatedBlock>
-				)
-			})}
+			{connections.map((item, i) => (
+				<AnimatedBlock type="FADE_IN" delay={0.1 * i} className="col-span-1" key={i}>
+					<ConnectionBlock data={item} />
+				</AnimatedBlock>
+			))}
 		</div>
 	)
 }
